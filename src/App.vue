e54463e253e39dae72b6d6f84ffb9e6331aac01f
Leo-Volkov <Leo.Volkov156@yandex.ru>
2024-05-20 08:05:53 +0300

    .gitattributes .gitignore .vscode/extensions.json README.md index.css index.html index.js jsconfig.json package-lock.json package.json public/favicon.ico src/App.vue src/components/HelloWorld.vue src/main.js vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="You did it!" />
+</template>
+
+<style>
+body {
+    background-color: rgb(255, 189, 199);
+}
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<template>
+    <h1>To Do List</h1>
+    <div>
+        <ul>
+            <li>
+                <div>
+                    <input type="text">
+                    <div class="li_redackt"></div>
+                    <div class="li_delit"></div>
+                </div>
+            </li>
+        </ul>
+        <div class="li_plus">+</div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
